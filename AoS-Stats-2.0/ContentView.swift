d2628862deda9de00edbd039de630ee374584c2d
Vilhelm Stokstad <v.stokstad@gmail.com>
2020-01-18 12:32:08 +0100

    Initial Commit

---
+++ AoS-Stats-2.0/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  AoS-Stats-2.0
+//
+//  Created by Vilhelm Stokstad on 2020-01-18.
+//  Copyright © 2020 Vilhelm Stokstad. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ AoS-Stats-2.0Tests/AoS_Stats_2_0Tests.swift
@@ -0,0 +1,34 @@
+//
+//  AoS_Stats_2_0Tests.swift
+//  AoS-Stats-2.0Tests
+//
+//  Created by Vilhelm Stokstad on 2020-01-18.
+//  Copyright © 2020 Vilhelm Stokstad. All rights reserved.
+//
+
+import XCTest
+@testable import AoS_Stats_2_0
+
+class AoS_Stats_2_0Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
